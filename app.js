ab2c6df29151be738fa23c2303c06a18a89fb840
German Martinez <germaaan@gmail.com>
2018-01-05 23:12:16 +0100

    Cambios en el servidor

--- app.js
+++ app.js
@@ -1,16 +1,44 @@
-"use strict";
-
 var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+let index = require('./routes/index');
+
 var app = express();
 
-var config = require("./app/config");
-var routes = require("./app/routes");
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-config(app);
-routes(app);
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-var server = app.listen(app.get("port"), app.get("ip"), function() {
-	console.log("Express server listening on " + app.get("ip") + ":" + app.get("port"));
+app.use('/', index);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-module.exports = server;
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
