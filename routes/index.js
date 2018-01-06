2faf8f084485a63d672294d8142aa03bef1ad2bf
German Martinez <germaaan@gmail.com>
2018-01-06 02:26:41 +0100

    Modifica ruta indice

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,30 @@
 var express = require('express');
 var router = express.Router();
-var debug = require('debug')('jasyp-c4p:route:reservations')
+var debug = require('debug')('jasyp-c4p:route:papers')
+const papers = require('../lib/papers');
+const Papers = require('../lib/schema/paper');
 
-
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'JASYP C4P' });
+});
+
+router.post('/', function(req, res, next) {
+  const paper = new Paper(req.body);
+  papers.create(paper)
+    .then(paperId => res.render('papers', {
+      success: true,
+      paperId
+    }))
+    .catch(err => {
+      debug(err.message, req.body);
+      res.status(400).render('papers', {
+        errors: [
+          err.message
+        ],
+        success: false,
+        submission: req.body
+      });
+    })
 });
 
 module.exports = router;
