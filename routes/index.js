7747b8ab1e2ec93258acc6f9719d0ac3a4a436c4
German Martinez <germaaan@gmail.com>
2018-01-06 02:01:50 +0100

    Añade schema

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var debug = require('debug')('jasyp-c4p:route:reservations')
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'JASYP C4P' });
 });
 
 module.exports = router;
