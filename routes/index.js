a193c245dce4bd5bf0d056929e3910f19e6dff8f
German Martinez <germaaan@gmail.com>
2018-01-14 02:51:34 +0100

    Añade página de edición y borrado

--- routes/index.js
+++ routes/index.js
@@ -5,23 +5,8 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", function(req, res, next) {
-	res.render("index", {
+	res.render("pages/index", {
 		title: "JASYP C4P"
-	});
-});
-
-router.get("/create", function(req, res, next) {
-	res.render("create", {
-		title: "JASYP C4P - Crear comunicaciones"
-	});
-});
-
-router.get("/list", function(req, res) {
-	models.Paper.findAll({}).then(function(papers) {
-		res.render("list", {
-			title: "JASYP C4P - Listar comunicaciones",
-			papers: papers
-		});
 	});
 });
 
--- routes/papers.js
+++ routes/papers.js
@@ -4,12 +4,68 @@ var models = require("../models");
 var express = require("express");
 var router = express.Router();
 
+router.get("/create", function(req, res, next) {
+	res.render("pages/create", {
+		title: "JASYP C4P - Crear comunicaciones"
+	});
+});
+
 router.post("/create", function(req, res) {
 	models.Paper.create({
 		title: req.body.title,
 		name: req.body.name,
 		email: req.body.email,
 		abstract: req.body.abstract
+	}).then(function() {
+		res.redirect("/");
+	});
+});
+
+router.get("/list", function(req, res) {
+	models.Paper.findAll({
+		attributes: ["id", "title", "name", "email", "abstract"]
+	}).then(function(papers) {
+		res.render("pages/list", {
+			title: "JASYP C4P - Listar comunicaciones",
+			papers: papers
+		});
+	});
+});
+
+router.get("/:paper_id", function(req, res) {
+	models.Paper.find({
+		attributes: ["id", "title", "name", "email", "abstract"],
+		where: {
+			id: req.params.paper_id
+		}
+	}).then(function(paper) {
+		res.render("pages/edit", {
+			title: "JASYP C4P - Editar comunicaciones",
+			paper: paper
+		});
+	});
+});
+
+router.post("/:paper_id/update", function(req, res) {
+	models.Paper.update({
+		title: req.body.title,
+		name: req.body.name,
+		email: req.body.email,
+		abstract: req.body.abstract
+	}, {
+		where: {
+			id: req.params.paper_id
+		}
+	}).then(function() {
+		res.redirect("/");
+	});
+});
+
+router.post("/:paper_id/delete", function(req, res) {
+	models.Paper.destroy({
+		where: {
+			id: req.params.paper_id
+		}
 	}).then(function() {
 		res.redirect("/");
 	});
